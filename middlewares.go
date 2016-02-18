7a90d457466a9ad49266c3ceb12734468e932b05
Kamaleshwar Nair <kamaleshbn@KBN.local>
2016-02-18 11:44:57 +0530

    improved CORS middlewares, OPTIONs is now handled separately

--- middlewares.go
+++ middlewares.go
@@ -11,24 +11,25 @@ type Middlewares struct{}
 // A basic Cors middleware definition.
 func (m Middlewares) Cors(ctx *Context, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if origin := r.Header.Get("Origin"); origin != "" {
-			allowedHeaders := r.Header.Get("Access-Control-Allow-Headers")
-			if allowedHeaders != "" {
-				allowedHeaders = "," + allowedHeaders
-			}
-			// Set response appropriate headers required for CORS
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Access-Control-Allow-Headers, Authorization, authKey, AuthKey, AuthToken, Token"+allowedHeaders)
-			// Stop here & return if it's a preflighted `OPTIONS` request
-		}
-		if r.Method == "OPTIONS" {
-			return
-		}
+		// Set response appropriate headers required for CORS
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Access-Control-Allow-Headers, Authorization, authKey, AuthKey, AuthToken, Token,"+r.Header.Get("Access-Control-Allow-Headers"))
 
 		next.ServeHTTP(w, r)
 	})
 }
 
-// ===
+// Cors middleware just for Options request - adding this helped remove the
+// request method check from Cors middleware
+func (m Middlewares) CorsOptions(ctx *Context, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set response appropriate headers required for CORS
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Access-Control-Allow-Headers, Authorization, authKey, AuthKey, AuthToken, Token,"+r.Header.Get("Access-Control-Allow-Headers"))
+		return
+	})
+}
