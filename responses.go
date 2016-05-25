f490b8bb03e9c0d02e5fe3974d16b8764c40d949
Kamaleshwar Nair <kamaleshbn@KBN.local>
2016-05-25 22:44:54 +0530

    minor changes to how responses are handled, now has a minor performance improvement

--- responses.go
+++ responses.go
@@ -2,6 +2,7 @@ package webgo
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -12,32 +13,41 @@ type ErrorData struct {
 	ErrDescription string
 }
 
+type dOutput struct {
+	Data   interface{} `json:"data"`
+	Status int         `json:"status"`
+}
+
+type errOutput struct {
+	Errors interface{} `json:"errors"`
+	Status int         `json:"status"`
+}
+
 // SendReponse is used to respond to any request (JSON response) based on the code, data etc.
 func SendResponse(w http.ResponseWriter, data interface{}, rCode int) {
 	w.WriteHeader(rCode)
-
-	// renderFile = false means response should be in JSON
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-	type dOutput struct {
-		Data   interface{} `json:"data"`
-		Status int         `json:"status"`
-	}
-
-	type errOutput struct {
-		Errors interface{} `json:"errors"`
-		Status int         `json:"status"`
-	}
-
-	if rCode < 400 {
-		// wrap the response data inside {"data": <data>}
-		data = &dOutput{data, rCode}
-	} else {
-		// wrap the response data inside {"errors": <data>}
-		data = &errOutput{data, rCode}
-	}
-
+	data = &dOutput{data, rCode}
 	// Encode data to json and send response
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		/*
+			In case of encoding error, send "internal server error" after
+			logging the actual error
+		*/
+		R500(w, struct {
+			errors []string
+		}{
+			[]string{"Internal server error."},
+		})
+	}
+}
+
+func SendError(w http.ResponseWriter, data interface{}, rCode int) {
+	w.WriteHeader(rCode)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	data = &errOutput{data, rCode}
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		/*
 			In case of encoding error, send "internal server error" after
@@ -71,8 +81,6 @@ func Render404(w http.ResponseWriter, tpl *template.Template) {
 	)
 }
 
-// For JSON response with appropriate response code
-
 // R200 - Successful/OK response
 func R200(w http.ResponseWriter, data interface{}) {
 	SendResponse(w, data, 200)
@@ -83,9 +91,10 @@ func R201(w http.ResponseWriter, data interface{}) {
 	SendResponse(w, data, 201)
 }
 
-// R204 - empty result
-func R204(w http.ResponseWriter, data interface{}) {
-	SendResponse(w, data, 204)
+// R204 - empty, no content
+func R204(w http.ResponseWriter) {
+	w.WriteHeader(204)
+	fmt.Fprint(w)
 }
 
 // R302 - Temporary redirect
@@ -95,30 +104,30 @@ func R302(w http.ResponseWriter, data interface{}) {
 
 // R400 - Invalid request, any incorrect/erraneous value in the request body
 func R400(w http.ResponseWriter, data interface{}) {
-	SendResponse(w, data, 400)
+	SendError(w, data, 400)
 }
 
 // R403 - Unauthorized access
 func R403(w http.ResponseWriter, data interface{}) {
-	SendResponse(w, data, 403)
+	SendError(w, data, 403)
 }
 
 // R404 - Resource not found
 func R404(w http.ResponseWriter, data interface{}) {
-	SendResponse(w, data, 404)
+	SendError(w, data, 404)
 }
 
 // R406 - Unacceptable header. For any error related to values set in header
 func R406(w http.ResponseWriter, data interface{}) {
-	SendResponse(w, data, 406)
+	SendError(w, data, 406)
 }
 
 // R451 - Resource taken down because of a legal request
 func R451(w http.ResponseWriter, data interface{}) {
-	SendResponse(w, data, 451)
+	SendError(w, data, 451)
 }
 
 // R500 - Internal server error
 func R500(w http.ResponseWriter, data interface{}) {
-	SendResponse(w, data, 500)
+	SendError(w, data, 500)
 }
