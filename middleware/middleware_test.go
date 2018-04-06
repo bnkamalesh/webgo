ac43a14f7a92270f361c09dba07bdff6d0a76656
Kamaleshwar <bn_kamalesh@yahoo.com>
2018-04-06 23:52:29 +0530

    added more tests

---
+++ middleware/middleware_test.go
@@ -0,0 +1,70 @@
+package middleware
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/bnkamalesh/webgo"
+)
+
+const baseapi = "http://127.0.0.1:9696"
+
+func TestMiddleware(t *testing.T) {
+	router, respRec := setup()
+	router.Use(CorsWrap)
+	router.Use(AccessLog)
+	url := baseapi
+	req, err := http.NewRequest(http.MethodGet, url, bytes.NewBuffer(nil))
+	if err != nil {
+		t.Log(err, url)
+		t.Fail()
+	}
+
+	router.ServeHTTP(respRec, req)
+
+	if respRec.Code != 200 {
+		t.Log(err, respRec.Code, url)
+		t.Fail()
+	}
+
+	h := respRec.Header().Get(headerAllowHeaders)
+	if h != allowHeaders {
+		t.Log("Expected ", allowHeaders, "\ngot", h)
+		t.Fail()
+	}
+}
+
+func setup() (*webgo.Router, *httptest.ResponseRecorder) {
+	// Initializing router with all the required routes
+	router := webgo.NewRouter(&webgo.Config{
+		Host:               "127.0.0.1",
+		Port:               "9696",
+		HTTPSPort:          "8443",
+		CertFile:           "tests/ssl/server.crt",
+		KeyFile:            "tests/ssl/server.key",
+		ReadTimeout:        15,
+		WriteTimeout:       60,
+		InsecureSkipVerify: true,
+	}, getRoutes())
+
+	return router, httptest.NewRecorder()
+}
+
+func getRoutes() []*webgo.Route {
+	return []*webgo.Route{
+		&webgo.Route{
+			Name:                    "root",         // A label for the API/URI
+			Method:                  http.MethodGet, // request type
+			Pattern:                 "/",
+			FallThroughPostResponse: true, // Pattern for the route
+			TrailingSlash:           true,
+			Handlers:                []http.HandlerFunc{handler}, // route handler
+		},
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	webgo.R200(w, "hello world")
+}
---
+++ responses_test.go
@@ -0,0 +1,131 @@
+package webgo
+
+import (
+	"html/template"
+	"testing"
+)
+
+func TestResponses(t *testing.T) {
+	_, respRec := setup()
+	R200(respRec, nil)
+	if respRec.Code != 200 {
+		t.Log("Expected response status 200, got", respRec.Code)
+		t.Fail()
+	}
+
+	_, respRec = setup()
+	R201(respRec, nil)
+	if respRec.Code != 201 {
+		t.Log("Expected response status 201, got", respRec.Code)
+		t.Fail()
+	}
+
+	_, respRec = setup()
+	R204(respRec)
+	if respRec.Code != 204 {
+		t.Log("Expected response status 204, got", respRec.Code)
+		t.Fail()
+	}
+
+	_, respRec = setup()
+	R302(respRec, nil)
+	if respRec.Code != 302 {
+		t.Log("Expected response status 302, got", respRec.Code)
+		t.Fail()
+	}
+
+	_, respRec = setup()
+	R400(respRec, nil)
+	if respRec.Code != 400 {
+		t.Log("Expected response status 400, got", respRec.Code)
+		t.Fail()
+	}
+
+	_, respRec = setup()
+	R403(respRec, nil)
+	if respRec.Code != 403 {
+		t.Log("Expected response status 403, got", respRec.Code)
+		t.Fail()
+	}
+
+	_, respRec = setup()
+	R404(respRec, nil)
+	if respRec.Code != 404 {
+		t.Log("Expected response status 404, got", respRec.Code)
+		t.Fail()
+	}
+
+	_, respRec = setup()
+	R406(respRec, nil)
+	if respRec.Code != 406 {
+		t.Log("Expected response status 406, got", respRec.Code)
+		t.Fail()
+	}
+
+	_, respRec = setup()
+	R451(respRec, nil)
+	if respRec.Code != 451 {
+		t.Log("Expected response status 451, got", respRec.Code)
+		t.Fail()
+	}
+
+	_, respRec = setup()
+	R500(respRec, nil)
+	if respRec.Code != 500 {
+		t.Log("Expected response status 500, got", respRec.Code)
+		t.Fail()
+	}
+
+}
+
+func TestSend(t *testing.T) {
+	_, respRec := setup()
+	Send(respRec, "text/html", "hello", 200)
+
+	if respRec.Code != 200 {
+		t.Log("Expected status 200, got", respRec.Code)
+		t.Fail()
+	}
+	str := respRec.Body.String()
+	if str != "hello" {
+		t.Log("Expected hello, got", str)
+		t.Fail()
+	}
+}
+
+func TestRender(t *testing.T) {
+	_, respResc := setup()
+
+	tmpl, err := template.New("sample").Parse(`hello world`)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+
+	Render(respResc, nil, 200, tmpl)
+
+	str := respResc.Body.String()
+	if str != `hello world` {
+		t.Log(str)
+		t.Fail()
+	}
+}
+func TestRender404(t *testing.T) {
+	_, respResc := setup()
+
+	tmpl, err := template.New("sample").Parse(`{{.ErrDescription}}`)
+	if err != nil {
+		t.Log(err)
+		t.Fail()
+		return
+	}
+
+	Render404(respResc, tmpl)
+
+	str := respResc.Body.String()
+	if str != `Sorry, the URL you requested was not found on this server... Or you&#39;re lost :-/` {
+		t.Log(str)
+		t.Fail()
+	}
+}
