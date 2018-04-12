ee2993efff85e7126fc18e0d38b9db964c5300f5
Kamaleshwar BN <bn_kamalesh@yahoo.com>
2018-04-12 22:11:52 +0530

    updated middleware test to fix URL

--- middleware/middleware_test.go
+++ middleware/middleware_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/bnkamalesh/webgo"
 )
 
-const baseapi = "http://127.0.0.1:9696"
+const baseapi = "http://127.0.0.1:9696/"
 
 func TestMiddleware(t *testing.T) {
 	router, respRec := setup()
