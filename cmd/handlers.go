d0f695c69e2ee9c1b16aefcba23b6e041f36e45a
bnkamalesh <bn_kamalesh@yahoo.com>
2022-03-13 16:59:27 +0530

    [-] removed os.ReadFile usage to make the sample app compatible with Go 1.13

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -34,11 +34,24 @@ func OriginalResponseWriterHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	out, err := os.ReadFile("./cmd/static/index.html")
+	fs, err := os.OpenFile("./cmd/static/index.html", os.O_RDONLY, 0600)
 	if err != nil {
 		webgo.SendError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	info, err := fs.Stat()
+	if err != nil {
+		webgo.SendError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	out := make([]byte, info.Size())
+	_, err = fs.Read(out)
+	if err != nil {
+		webgo.SendError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	_, _ = w.Write(out)
 }
 
