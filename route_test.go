622e5fc0fa9e6de9b2ad8ece8b8f720770e05f0f
bnkamalesh <bn_kamalesh@yahoo.com>
2022-06-14 11:10:04 +0530

    [patch] better URI matching, improved performance

--- route_test.go
+++ route_test.go
@@ -3,6 +3,7 @@ package webgo
 import (
 	"fmt"
 	"net/http"
+	"reflect"
 	"testing"
 )
 
@@ -48,3 +49,153 @@ func TestRouteGroupsPathPrefix(t *testing.T) {
 }
 
 func dummyHandler(w http.ResponseWriter, r *http.Request) {}
+
+func BenchmarkMatchWithWildcard(b *testing.B) {
+	route := Route{
+		Name:                    "widlcard",
+		Method:                  http.MethodGet,
+		TrailingSlash:           true,
+		FallThroughPostResponse: true,
+		Pattern:                 "/:w*/static1/:myvar/:w2*",
+		Handlers:                []http.HandlerFunc{dummyHandler},
+	}
+
+	uri := "/hello/world/how/are/you/static1/hello2/world2/how2/are2/you2/static2"
+	err := route.init()
+	if err != nil {
+		b.Error(err)
+		return
+	}
+
+	for i := 0; i < b.N; i++ {
+		ok, _ := route.matchPath(uri)
+		if !ok {
+			b.Errorf("Expected match, got no match")
+			break
+		}
+	}
+}
+
+func TestMatchWithWildcard(t *testing.T) {
+	route := Route{
+		Name:                    "widlcard",
+		Method:                  http.MethodGet,
+		TrailingSlash:           true,
+		FallThroughPostResponse: true,
+		Pattern:                 "/:w*/static1/:myvar/:w2*/static2",
+		Handlers:                []http.HandlerFunc{dummyHandler},
+	}
+	err := route.init()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	uri := "/hello/world/how/are/you/static1/hello2/world2/how2/are2/you2/static2"
+	wantParams := map[string]string{
+		"w":     "hello/world/how/are/you",
+		"myvar": "hello2",
+		"w2":    "world2/how2/are2/you2",
+	}
+	matched, params := route.matchPath(uri)
+	if !matched {
+		t.Errorf("Expected match, got no match")
+		return
+	}
+	if !reflect.DeepEqual(params, wantParams) {
+		t.Errorf("Expected params %v, got %v", wantParams, params)
+		return
+	}
+
+	t.Run("no match", func(t *testing.T) {
+		route := Route{
+			Name:                    "widlcard",
+			Method:                  http.MethodGet,
+			TrailingSlash:           true,
+			FallThroughPostResponse: true,
+			Pattern:                 "/:w*/static1/:myvar/:w2*/static2",
+			Handlers:                []http.HandlerFunc{dummyHandler},
+		}
+		err := route.init()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		uri := "/hello/world/how/are/you/static2/hello2/world2/how2/are2/you2/static2"
+		wantParams := map[string]string{}
+		matched, params := route.matchPath(uri)
+		if matched {
+			t.Errorf("Expected no match, got match")
+			return
+		}
+		if reflect.DeepEqual(params, wantParams) {
+			t.Errorf("Expected params %v, got %v", wantParams, params)
+			return
+		}
+	})
+	t.Run("match with more params", func(t *testing.T) {
+		route := Route{
+			Name:                    "widlcard",
+			Method:                  http.MethodGet,
+			TrailingSlash:           true,
+			FallThroughPostResponse: true,
+			Pattern:                 "/:w*/static1/:myvar/:w2*/static2/:myvar2/:w3*/static3",
+			Handlers:                []http.HandlerFunc{dummyHandler},
+		}
+		err := route.init()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		uri := "/hello/world/how/are/you/static1/hello2/world2/how2/are2/you2/static2/hello3/world3/how3/are3/you3/static3"
+		wantParams := map[string]string{
+			"w":      "hello/world/how/are/you",
+			"myvar":  "hello2",
+			"w2":     "world2/how2/are2/you2",
+			"myvar2": "hello3",
+			"w3":     "world3/how3/are3/you3",
+		}
+		matched, params := route.matchPath(uri)
+		if !matched {
+			t.Errorf("Expected match, got no match")
+			return
+		}
+		if !reflect.DeepEqual(params, wantParams) {
+			t.Errorf("Expected params %v, got %v", wantParams, params)
+			return
+		}
+	})
+	t.Run("match - end with wildcard", func(t *testing.T) {
+		route := Route{
+			Name:                    "widlcard",
+			Method:                  http.MethodGet,
+			TrailingSlash:           true,
+			FallThroughPostResponse: true,
+			Pattern:                 "/:w*/static1/:myvar/:w2*",
+			Handlers:                []http.HandlerFunc{dummyHandler},
+		}
+		err := route.init()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		uri := "/hello/world/how/are/you/static1/hello2/world2/how2/are2/you2/static2"
+		wantParams := map[string]string{
+			"w":     "hello/world/how/are/you",
+			"myvar": "hello2",
+			"w2":    "world2/how2/are2/you2/static2",
+		}
+		matched, params := route.matchPath(uri)
+		if !matched {
+			t.Errorf("Expected match, got no match")
+			return
+		}
+		if reflect.DeepEqual(params, wantParams) {
+			t.Errorf("Expected params %v, got %v", wantParams, params)
+			return
+		}
+	})
+}
