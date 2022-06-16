7b9ea02bfcf0226f513dc1d9c729f68fc462cea6
Kamaleshwar <1092882+bnkamalesh@users.noreply.github.com>
2022-06-16 19:23:46 +0530

    Refactor (#43)
    [patch] better URI matching, reduced allocations
    [patch] resources are released even in case of panic (deferred)
    [-] added benchmark

--- webgo_test.go
+++ webgo_test.go
@@ -81,3 +81,22 @@ func TestErrorHandling(t *testing.T) {
 		t.Fatalf("expected err %v, got %v", err, gotErr)
 	}
 }
+
+func BenchmarkRouter(b *testing.B) {
+	GlobalLoggerConfig(nil, nil, LogCfgDisableDebug, LogCfgDisableInfo, LogCfgDisableWarn)
+	t := &testing.T{}
+	router, err := setup(t, "1595")
+	if err != nil {
+		b.Error(err)
+		return
+	}
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest(http.MethodGet, "/a/b/-/c/~/d/./e", nil)
+	for i := 0; i < b.N; i++ {
+		router.ServeHTTP(w, r)
+		if w.Result().StatusCode != http.StatusOK {
+			b.Error("expected status 200, got", w.Result().StatusCode)
+			return
+		}
+	}
+}
